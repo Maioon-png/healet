51fe73b1ef691d54398b892ae1308c159df4a5eb
Maioon-png <mi_mkb.12_5@icloud.com>
2020-04-18 13:44:22 +0900

    add_function-comment

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -14,6 +14,8 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
+    @comment = Comment.new
+    @comments = @topic.comments.includes(:user)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root 'top#index'
   resources :users, only: :index
   resources :weights, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :meals, only: [:index, :new, :create, :edit, :update, :destroy]
-  resources :topics, only: [:index, :new, :create, :show]
+  resources :topics, only: [:index, :new, :create, :show] do
+    resources :comments, only: :create
+  end
+
 end
