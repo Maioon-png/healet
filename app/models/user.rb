c7131267fcdd70ebc9112992198a4c1606ec67d6
Maioon-png <mi_mkb.12_5@icloud.com>
2020-07-19 19:41:14 +0900

    create gest-login-function

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
   has_many :meals
   has_many :topics
   has_many :comments
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.start_weight = 67
+      user.target_weight = 58
+      user.name = "ゲストユーザー"
+
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   root 'top#index'
   resources :users, only: :index
   resources :weights, only: [:index, :new, :create, :edit, :update, :destroy]
