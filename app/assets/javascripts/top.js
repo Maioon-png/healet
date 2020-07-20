58f667fed6a196be84c7fdf38b632617ec1aecb1
Maioon-png <mi_mkb.12_5@icloud.com>
2020-07-20 14:50:23 +0900

    add jquery-bgswitcher

---
+++ app/assets/javascripts/top.js
@@ -0,0 +1,6 @@
+$(function() {
+  $('.bg-slider').bgSwitcher({
+    images: ['/assets/diet1.jpg','/assets/diet2.jpg','/assets/diet3.jpg','/assets/diet4.jpg'], // 切り替える背景画像を指定
+    interval: 3000
+  });
+});
