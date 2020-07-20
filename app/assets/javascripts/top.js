de2666dad52ecd971e08f519a44d6f13aba72486
Maioon-png <mi_mkb.12_5@icloud.com>
2020-07-20 17:09:17 +0900

    move images to /public/images/

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,6 +1,6 @@
 $(function() {
   $('.bg-slider').bgSwitcher({
-    images: ['/assets/diet1.jpg','/assets/diet2.jpg','/assets/diet3.jpg','/assets/diet4.jpg'], // 切り替える背景画像を指定
+    images: ['/images/diet1.jpg','/images/diet2.jpg','/images/diet3.jpg','/images/diet4.jpg'], // 切り替える背景画像を指定
     interval: 3000
   });
 });
