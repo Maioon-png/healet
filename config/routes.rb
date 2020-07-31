79a8571130294eab09808e1df96584ac5af39244
Maioon-png <mi_mkb.12_5@icloud.com>
2020-07-31 23:48:03 +0900

    add routing edit & update in users

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
   root 'top#index'
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :weights, only: [:index, :create, :edit, :update, :destroy]
   resources :meals, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :topics do
