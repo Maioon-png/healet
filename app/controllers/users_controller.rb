efa95956f62c74418441d2568ee6d21611d209c0
Maioon-png <mi_mkb.12_5@icloud.com>
2020-08-01 23:55:35 +0900

    add user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,13 +15,17 @@ class UsersController < ApplicationController
   end
 
   def update
-    # user = User.find(params[:id])
-    # user.update(weight_params)
+    user = User.find(params[:id])
+    user.update(user_params)
     # redirect_to users_path
   end
 
 
   private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :target_weight).merge(user_id: current_user.id)
+  end
 
   def move_to_index
     unless user_signed_in?
